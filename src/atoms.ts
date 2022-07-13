58ab9edb5126274c1d1d99a7280eecca71fc03fc
Elena Andrés <elena@kraftlauget.no>
2022-07-13 22:07:05 +0200

    Tailwind styling

--- src/atoms.ts
+++ src/atoms.ts
@@ -2,7 +2,7 @@ import { atom } from "jotai";
 import { atomWithStorage } from 'jotai/utils'
 
 // Create your atoms and derivatives
-const readingSuggestionsAtom = atomWithStorage<string>('readingSuggestions', "Read a blue book, Read a book under 200 pages");
+const readingSuggestionsAtom = atomWithStorage<string>('readingSuggestions', "Read a blue book, Read a book under 200 pages, Read a book with a plant on the cover");
 const readingSuggestionsListAtom = atom<string[]>((get) => get(readingSuggestionsAtom).split(","));
 
 export {readingSuggestionsAtom, readingSuggestionsListAtom};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{js,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
+    colors: {
+      darkerBlue: '#05739C',
+      lighterBlue: '#DEF6FF',
+      blue: '#45CDFF',
+      brown: '#9E6E3A',
+      beige: '#EBBA86',
+      white: '#fff',
+      black: '#000',
+    },
   },
   plugins: [],
 }
